419e50af04e2b86d26b744c33d82eb923f79c873
santh0shh <mr.santhosh116@gmail.com>
2026-06-12 02:55:26 +0530

    navigation

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -8,7 +8,9 @@ export default function Intro() {
           </h1>
           <h1 className="text-8xl font-extrabold text-red-600 tracking-widest">RED ROOM</h1>
   
-          <button className="mt-10 px-8 py-3 border border-red-600 text-red-500 hover:bg-red-600 hover:text-white transition duration-300 rounded-lg">
+          <button className="mt-10 px-8 py-3 border border-red-600 text-red-500 hover:bg-red-600 hover:text-white transition duration-300 rounded-lg"
+          onClick={()=>navigate("/main")}
+          >
             Enter
           </button>
         </div>
